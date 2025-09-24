9cec10f4c53eca4b113f746d84a2aaf6ad449ed3
mikitaslimianiou <105208458+mikitaslimianiou@users.noreply.github.com>
2025-09-24 23:24:42 +0300

    Update start_and_reload_nginx.sh

--- scripts/start_and_reload_nginx.sh
+++ scripts/start_and_reload_nginx.sh
@@ -1,1 +1,14 @@
+#!/bin/bash
+set -e
 
+cd /absolute path to your app
+
+docker compose up -d
+
+# Get the container ID for the 'nginx' service
+NGINX_CONTAINER_ID=$(docker compose ps -q nginx)
+
+# If the container is running, reload nginx inside it
+if [ -n "$NGINX_CONTAINER_ID" ]; then
+  docker exec "$NGINX_CONTAINER_ID" nginx -s reload || true
+fi
