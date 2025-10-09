1b755f41c212232bb69b8c852e4c2940e9c098a2
mikitaslimianiou <105208458+mikitaslimianiou@users.noreply.github.com>
2025-10-09 22:00:25 +0300

    Update start_application.sh

--- scripts/start_application.sh
+++ scripts/start_application.sh
@@ -2,6 +2,8 @@
 set -e
 
 cd /testRepo/app
+snap install aws-cli --classic
+aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 124355683173.dkr.ecr.us-east-1.amazonaws.com
 
 docker compose up -d
 
